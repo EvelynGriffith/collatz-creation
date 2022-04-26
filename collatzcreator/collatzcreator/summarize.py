39ebd038b46be213cbc4339d0dfeb312d62dec84
EvelynGriffith <griffith01@allegheny.edu>
2022-04-26 15:20:56 -0400

    adding code to the summarize.py file

--- collatzcreator/collatzcreator/summarize.py
+++ collatzcreator/collatzcreator/summarize.py
@@ -6,28 +6,81 @@ import copy
 
 
 def compute_mean(numbers: List[int]) -> float:
-    # TODO: add a complete implementation of this function
+    # add a complete implementation of this function
+    """Compute the mean for a list called numbers which contains floats."""
+    # sum the list of the numbers
+    Sum = sum(numbers)
+    # determine the length of the list of numbers
+    Size = len(numbers)
+    # as long as the computation will not be an
+    # undefined division by zero, compute the mean
+    for i in numbers:
+        if i in numbers != 0:
+            mean = Sum / Size
+            return mean
+    # https://stackoverflow.com/questions/58400652/average-returning-a-value-even-when-list-is-empty
+    # if the list was empty, then return a mean that is "not a number"
+    return float("NaN")
 
 
 def compute_median(numbers: List[int]) -> float:
-    # TODO: add a complete implementation of this function
-    # sort the numbers in an "in place" fashion
-    # after creating a "deep copy" of the numbers
-    # array. Note that this is necessary because
-    # it is not appropriate to sort the actual
-    # list of numbers since this will potentially destroy
-    # any other analysis or visualization done on list
-    # Reference:
-    # https://stackoverflow.com/questions/17873384/how-to-deep-copy-a-list
+    """Compute the median of the list called numbers."""
+    n = len(numbers)
+    if n == 0:
+        return float("NaN")
+    numbers.sort()
+    # as long as the computation will not be an
+    # undefined division by zero, compute the median
+    # case: the count of the values is even
+    if n % 2 == 0:
+        # get the two indices that are before and after the middle
+        # convert to an integer to prepare for indexing
+        # adjust for the fact that lists index starting at 0
+        m1 = n / 2
+        m2 = (n / 2) + 1
+        m1 = int(m1) - 1
+        m2 = int(m2) - 1
+        # compute the median value
+        median = (numbers[m1] + numbers[m2]) / 2
+    else:
+        # case: the count of the values is odd
+        m = (n + 1) / 2
+        # convert to an integer to prepare for indexing
+        # adjust for the fact that lists index starting at 0
+        m = int(m) - 1
+        median = numbers[m]
+        # if the list was empty, then return a median that is "not a number"
+    # return the computed median value
+    return median
 
 
 def compute_difference(numbers: List[int]) -> List[float]:
-    # TODO: add a complete implementation of this function
+    """Compute the difference of the list called numbers."""
+    # add a complete implementation of this function
+    mean = compute_mean(numbers)
+    diff = []
+    for num in numbers:
+        diff.append(num - mean)
+    return diff
 
 
 def compute_variance(numbers: List[int]) -> float:
-    # TODO: add a complete implementation of this function
+    """Compute variance for list called numbers."""
+    # add a complete implementation of this function
+    diff = compute_difference(numbers)
+    squared_diff = []
+    for d in diff:
+        squared_diff.append(d ** 2)
+    sum_squared_diff = sum(squared_diff)
+    variance = sum_squared_diff / len(numbers)
+    return variance
 
 
 def compute_standard_deviation(numbers: List[int]) -> float:
-    # TODO: add a complete implementation of this function
+    """Compute standard deviation for a list called numbers."""
+    # add a complete implementation of this function.
+    vari = compute_variance(numbers)
+    # calculate the standard deviation as the square root of the variance
+    stand_dev = vari ** 0.5
+    # return the calculated standard devision of the list of numbers
+    return stand_dev
